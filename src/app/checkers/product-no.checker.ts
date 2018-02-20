538046a9162b5d03675a453759da7dfffd6d69be
Oomusou Xiao <oomusou@gmail.com>
2018-02-20 10:45:50 +0800

    ProductNoChecker by if with Default Value

--- src/app/checkers/product-no.checker.ts
+++ src/app/checkers/product-no.checker.ts
@@ -3,20 +3,14 @@ import { Injectable } from '@angular/core';
 @Injectable()
 export class ProductNoChecker {
   check(productNo: number): boolean {
-    let result: boolean;
+    let result = false;
 
     if (Number.isInteger(productNo)) {
       if ((productNo % 2) === 0) {
         if ((productNo % 3) === 0) {
           result = true;
-        } else {
-          result = false;
         }
-      } else {
-        result = false;
       }
-    } else {
-      result = false;
     }
 
     return result;
