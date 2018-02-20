4549b226749ae123fb51e55f341b879080db866a
Oomusou Xiao <oomusou@gmail.com>
2018-02-20 10:40:22 +0800

    Setup Wallaby.js and Unit Test

--- src/app/checkers/product-no.checker.spec.ts
+++ src/app/checkers/product-no.checker.spec.ts
@@ -1,14 +1,38 @@
-import { inject, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 import { ProductNoChecker } from './product-no.checker';
 
-describe('ProductNoCheckerService', () => {
+describe('ProductNoService', () => {
+  let productNoChecker: ProductNoChecker;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [ProductNoChecker]
     });
+
+    productNoChecker = TestBed.get(ProductNoChecker);
   });
 
-  it('should be created', inject([ProductNoChecker], (service: ProductNoChecker) => {
-    expect(service).toBeTruthy();
-  }));
+  it('should be created', () => {
+    expect(productNoChecker).toBeTruthy();
+  });
+
+  it('當ProductNo為整數，且為2與3的倍數，應回傳 true', () => {
+    const productNo = 6;
+    expect(productNoChecker.check(productNo)).toBeTruthy();
+  });
+
+  it('當ProductNo為整數，為2的倍數但不是3的倍數，應回傳 false', () => {
+    const productNo = 2;
+    expect(productNoChecker.check(productNo)).toBeFalsy();
+  });
+
+  it('當ProductNo為整數，但不是2的倍數，應回傳 false', () => {
+    const productNo = 1;
+    expect(productNoChecker.check(productNo)).toBeFalsy();
+  });
+
+  it('當ProductNo不是整數，應回傳 false', () => {
+    const productNo = parseInt('t1', 10);
+    expect(productNoChecker.check(productNo)).toBeFalsy();
+  });
 });
